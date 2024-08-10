7210bde184458dc73d0726ad7dec4b444f4cb0af
brgltd <brgltdxyz@gmail.com>
2024-08-10 17:54:59 -0300

    feat: add html for healthcheck page

--- app/healthcheck/page.tsx
+++ app/healthcheck/page.tsx
@@ -1,11 +1,42 @@
-"use client"
+"use client";
 
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
 
 export default function Healthcheck() {
+	const [status, setStatus] = useState("isIdle");
+
+	const [pingMessage, setPingMessage] = useState();
+	const [pongMessage, setPongMessage] = useState();
+
+	const [fromChain, setFromChain] = useState();
+	const [toChain, setToChain] = useState();
+
+	const {
+		mutateAsync: execute,
+		isError,
+		isPending,
+		isSuccess,
+		error,
+	} = useMutation({
+		mutationFn: async () => {
+			try {
+				setPongMessage(undefined);
+			} catch (error) {}
+		}
+	})
+
+	const sfn = {
+		isIdle: (
+			<button>
+				send ping and receive pong
+			</button>
+		)
+	};
+
 	return (
 		<div>
-			<h1>test</h1>
 			<ConnectButton
 				chainStatus="none"
 				showBalance={false}
@@ -14,6 +45,20 @@ export default function Healthcheck() {
 					largeScreen: "full",
 				}}
 			/>
+			<div>
+				<label htmlFor="ping">ping</label>
+				<input
+					id="ping"
+					placeholder="write your message"
+					value={pingMessage}
+					onChange={(e) => setPingMessage(e.target.value)}
+				/>
+			</div>
+			{sfn[status]}		
+			<div>
+				<div>pong</div>
+				<div>{pongMessage || "no pong message"}</div>
+			</div>
 		</div>
 	);
 }
