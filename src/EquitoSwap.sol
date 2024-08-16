17a8b4fccb89fe65f06c93083b02bb5abc455c3b
brgltd <brgltdxyz@gmail.com>
2024-08-16 09:00:36 -0300

    feat: add initial code for bridge native tokens

--- src/EquitoSwap.sol
+++ src/EquitoSwap.sol
@@ -5,8 +5,43 @@ import {EquitoApp} from "equito/src/EquitoApp.sol";
 import {bytes64, EquitoMessage, EquitoMessageLibrary} from "equito/src/libraries/EquitoMessageLibrary.sol";
 
 contract EquitoSwap  is EquitoApp {
+	struct TokenAmount {
+		address token;
+		uint256 amount;
+		address recipient;
+	}
+
+    /// @dev The address used to represent the native token.
+    address internal constant NATIVE_TOKEN =
+        0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+	error InsufficientValueSent();
+
 	/// @notice Inittializes the contract with the router contract.
 	/// @param _router The address of the equito router contract.
 	constructor(address _router) EquitoApp(_router) {}
+
+	function bridgeNative(
+		uint256 destinationChainSelector,
+		uint256 sourceAmount
+	) external payable {
+		if (sourceAmount > msg.value) {
+			revert InsufficientValueSent();
+		}
+		uint256 fee = msg.value - sourceAmount;
+		uint256 destinationAmount = sourceAmount - fee;
+
+		TokenAmount memory tokenAmount = TokenAmount({
+			token: NATIVE_TOKEN,
+			amount: destinationAmount,
+			recipient: msg.sender
+		});
+
+		bytes32 messageHash = router.sendMessage{value: fee}(
+			peers[destinationChainSelector],
+			destinationChainSelector,
+			abi.encode(tokenAmount)
+		);
+	}
 }
 
