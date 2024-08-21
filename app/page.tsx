d31b90c50346cc848a5c6e03f569ba0ee8deb30f
brgltd <brgltdxyz@gmail.com>
2024-08-21 10:24:29 -0300

    feat: add connect button in swap page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,41 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { 
 	useSwitchChain,
 	useContractWrite,
-	usePrepareContractWrite
+	usePrepareContractWrite,
+	useAccount,
 } from "wagmi";
 import { prepareContractWrite } from "viem";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { chains } from "../utils/chains";
+import { Config } from "../config";
+import erc20Abi from "../abis/erc20.json";
 
 // TODO: replace with chain select.
 const ethereumChain = chains.find((chain) => chain.name === "Ethereum Sepolia");
 const arbitrumChain = chains.find((chain) => chain.name === "Arbitrum Sepolia");
 
 export default function Page() {
+	const [isClient, setIsClient] = useState(false);
+
 	const [inputAmount, setInputAmount] = useState("");
 
 	const { switchChainAsync } = useSwitchChain();
+
+	const { address } = useAccount();
+
+	/* const { config } = usePrepareContractWrite({ */
+	/* 	address: Config.Link_EthereumSepolia, */
+	/* 	abi: erc20Abi, */
+	/* 	functionName: "approve", */
+	/* 	args: [spenderAddress, '1000000000000000000'], */
+	/* }); */
+
+	useEffect(() => {
+		setIsClient(true);
+	}, []);
 
 	const onClickSwap = async () => {
 		try {
@@ -32,6 +51,17 @@ export default function Page() {
 
 	return (
 		<>
+			<ConnectButton
+				chainStatus="none"
+				showBalance={false}
+				accountStatus={{
+					smallScreen: "avatar",
+					largeScreen: "full",
+				}}
+			/>
+			{isClient && address ? `address: ${address}` : "not connected"}
+			<br />
+
 			{/* assume ether from ethereum to arbitrum */}
 			<label htmlFor="input-token">enter input amount</label>
 			<input
