b5fd13b6e0ada5ffecd66d51c7e8ddfc7bf6fa75
brgltd <brgltdxyz@gmail.com>
2024-08-27 07:31:47 -0300

    feat: healthcheck by sending text

--- src/Healthcheck.sol
+++ src/Healthcheck.sol
@@ -9,7 +9,7 @@ import {bytes64, EquitoMessage} from "equito/src/libraries/EquitoMessageLibrary.
 contract Healthcheck is EquitoApp {
 	event MessageSent(uint256 indexed destinationChainSelector, bytes32 messageHash);
 
-	event MessageReceived(uint256 indexed sourceChainSelector, string message);
+	event MessageReceived(uint256 indexed sourceChainSelector, string text);
 
 	constructor(address _router) EquitoApp(_router) {}
 	
@@ -17,7 +17,7 @@ contract Healthcheck is EquitoApp {
 		uint256 destinationChainSelector,
 		string calldata message
 	) external payable {
-		bytes64 receiver = peers[destinationChainSelector];
+		bytes64 memory receiver = peers[destinationChainSelector];
 		bytes memory messageData = abi.encode(message);
 		bytes32 messageHash = router.sendMessage{value: msg.value}(
 			receiver,
@@ -31,9 +31,8 @@ contract Healthcheck is EquitoApp {
 		EquitoMessage calldata message,
 		bytes calldata messageData
 	) internal override {
-		string memory message = abi.decode(messageData, string);
-		// string in the event? does that work?
-		emit MessageReceived(message.sourceChainSelector, message);
+		(string memory text) = abi.decode(messageData, (string));
+		emit MessageReceived(message.sourceChainSelector, text);
 	}
-  }
 }
+
