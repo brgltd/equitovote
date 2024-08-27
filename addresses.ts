a9bf519190f80c30e1adadeadcbfc571bb257b12
brgltd <brgltdxyz@gmail.com>
2024-08-27 08:11:22 -0300

    feat: deploy healthcheck to arbitrum sepolia

--- addresses.ts
+++ addresses.ts
@@ -5,7 +5,7 @@ export const Addresses = {
 
   // Deployed contracts of Healthcheck
   Healthcheck_EthereumSepolia_V1: "0xDb8a55b811DEBBe5cd28a1db7E78f0fE5d282862",
-  Healthcheck_ArbitrumSepolia_V1: "",
+  Healthcheck_ArbitrumSepolia_V1: "0xCD7949891D3075EF8681b9624746Ea78a5C27aa4",
 
   // Deployed contracts of EquitoVote
   EquitoVote_EthereumSepolia_V1: "",
