215b07dd70f4e101f62f9e451625dc37876afbfb
brgltd <brgltdxyz@gmail.com>
2024-09-03 19:02:48 -0300

    debug: fetch erc20 token name for proposal data rendering

--- app/vote/[id]/page.tsx
+++ app/vote/[id]/page.tsx
@@ -4,36 +4,94 @@ import { useMemo } from "react";
 import { destinationChain } from "@/utils/chains";
 import { useReadContract, useWriteContract } from "wagmi";
 import { buildProposalFromArray } from "@/utils/helpers";
-import { ProposalDataItem } from "@/types";
+import { FormattedProposal, ProposalDataItem, ProposalResponse } from "@/types";
 import equitoVote from "@/out/EquitoVote.sol/EquitoVote.json";
 import { Addresses, AddressesPerChain } from "@/addresses";
 import { useEquitoVote } from "@/providers/equito-vote-provider";
+import { Address } from "viem";
+import { config } from "@/utils/wagmi";
+import { waitForTransactionReceipt } from "@wagmi/core";
+import erc20Abi from "@/abis/erc20.json";
 
 const equitoVoteAbi = equitoVote.abi;
 
-export default function Vote({ params }: { params: { id: string } }) {
-  const { data: proposal, isLoading: isLoadingProposal } = useReadContract({
-    address: destinationChain.equitoVoteContract,
-    abi: equitoVoteAbi,
-    functionName: "proposals",
-    args: [params.id],
-    chainId: destinationChain.definition.id,
-  });
+enum VoteOption {
+  Yes = 0,
+  No = 1,
+  Abstain = 2,
+}
 
-  const { writeContractAsync } = useWriteContract();
+export default function Vote({ params }: { params: { id: string } }) {
+  const { id: proposalId } = params;
 
   const { sourceChain } = useEquitoVote();
 
-  // const voteOnProposal = async () => {
-  //   const hash = await writeContractAsync({
-  //     // address: AddressesPerChain.
-  //   });
-  // };
+  const { writeContractAsync } = useWriteContract();
 
-  const formattedProposal = useMemo(
-    () => buildProposalFromArray(proposal as ProposalDataItem[]),
+  const { data: proposalData, isLoading: isLoadingProposal } = useReadContract({
+    address: destinationChain.equitoVoteContract,
+    abi: equitoVoteAbi,
+    functionName: "proposals",
+    args: [proposalId],
+    chainId: destinationChain.definition.id,
+  });
+  const proposal = proposalData as ProposalDataItem[];
+
+  //@ts-ignore
+  const formattedProposal: FormattedProposal = useMemo(
+    () => buildProposalFromArray(proposal),
     [proposal],
   );
+
+  const {
+    data: tokenNameData,
+    isLoading: isLoadingTokenName,
+    error,
+  } = useReadContract({
+    address: formattedProposal.erc20 as Address,
+    abi: erc20Abi,
+    functionName: "name",
+    chainId: sourceChain?.definition.id,
+    query: { enabled: !!sourceChain },
+  });
+  console.log(tokenNameData);
+  console.log(error);
+  const tokenName = tokenNameData as string;
+
+  const approveERC20 = async () => {
+    const hash = await writeContractAsync({
+      address: formattedProposal.erc20 as Address,
+      abi: erc20Abi,
+      functionName: "approve",
+      args: [sourceChain?.equitoVoteContract, "25000000000000000000"],
+    });
+    await waitForTransactionReceipt(config, { hash });
+  };
+
+  const voteOnProposal = async (voteOption: VoteOption) => {
+    const hash = await writeContractAsync({
+      address: sourceChain?.equitoVoteContract as Address,
+      abi: equitoVoteAbi,
+      functionName: "voteOnProposal",
+      args: [
+        destinationChain.chainSelector,
+        proposalId,
+        1,
+        voteOption,
+        formattedProposal.erc20,
+      ],
+      chainId: sourceChain?.definition.id,
+    });
+    return waitForTransactionReceipt(config, {
+      hash,
+      chainId: sourceChain?.definition.id,
+    });
+  };
+
+  const onClickVoteOnProposal = async (voteOption: VoteOption) => {
+    // approve erc20
+    // voteOnProposal()
+  };
 
   return (
     <div>
@@ -53,9 +111,26 @@ export default function Vote({ params }: { params: { id: string } }) {
             <div>description {formattedProposal.description}</div>
             <div>id {formattedProposal.id}</div>
           </div>
-          <button className="block">Yes</button>
-          <button className="block">No</button>
-          <button className="block">Abstain</button>
+          <div>token name: {tokenName}</div>
+          <div>token balance: </div>
+          <button
+            onClick={() => onClickVoteOnProposal(VoteOption.Yes)}
+            className="block"
+          >
+            Yes
+          </button>
+          <button
+            onClick={() => onClickVoteOnProposal(VoteOption.No)}
+            className="block"
+          >
+            No
+          </button>
+          <button
+            onClick={() => onClickVoteOnProposal(VoteOption.Abstain)}
+            className="block"
+          >
+            Abstain
+          </button>
         </div>
       )}
     </div>
