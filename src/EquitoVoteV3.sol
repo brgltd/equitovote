a8461910b71eb106d09d68ae423a42233bf18482
brgltd <brgltdxyz@gmail.com>
2024-09-08 11:23:24 -0300

    feat: normalize num of votes with erc20 decimals

--- src/EquitoVoteV3.sol
+++ src/EquitoVoteV3.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.23;
 import {EquitoApp} from "equito/src/EquitoApp.sol";
 import {bytes64, EquitoMessage} from "equito/src/libraries/EquitoMessageLibrary.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
@@ -82,10 +83,7 @@ contract EquitoVote is EquitoApp, ReentrancyGuard {
         address[] tokenAddresses
     );
 
-    event TokenDataDeleted(
-        bytes32 tokenNameHash,
-        uint256[] chainSelectors
-    );
+    event TokenDataDeleted(bytes32 tokenNameHash, uint256[] chainSelectors);
 
     // --- errors ---
 
@@ -172,6 +170,7 @@ contract EquitoVote is EquitoApp, ReentrancyGuard {
         address tokenAddress
     ) external payable nonReentrant {
         balances[msg.sender][proposalId] += numVotes;
+
         IERC20(tokenAddress).safeTransferFrom(
             msg.sender,
             address(this),
@@ -179,15 +178,21 @@ contract EquitoVote is EquitoApp, ReentrancyGuard {
         );
 
         bytes64 memory receiver = peers[destinationChainSelector];
+
+        uint256 normalizedNumVotes = numVotes /
+            IERC20Metadata(tokenAddress).decimals();
+
         Proposal memory emptyNewProposal;
+
         bytes memory messageData = abi.encode(
             OperationType.VoteOnProposal,
             proposalId,
-            numVotes,
+            normalizedNumVotes,
             voteOption,
             tokenAddress,
             emptyNewProposal
         );
+
         bytes32 messageHash = router.sendMessage{value: msg.value}(
             receiver,
             destinationChainSelector,
@@ -238,7 +243,7 @@ contract EquitoVote is EquitoApp, ReentrancyGuard {
     function deleteTokenData(
         bytes32 tokenNameHash,
         uint256[] memory chainSelectors
-    )  external onlyOwner {
+    ) external onlyOwner {
         uint256 chainSelectorsLength = chainSelectors.length;
         for (uint256 i = 0; i < chainSelectorsLength; i = uncheckedInc(i)) {
             tokenData[tokenNameHash][chainSelectors[i]] = address(0);
