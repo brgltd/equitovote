93dc7143f2f87622324f8b61c9fd031bdd52eef7
brgltd <brgltdxyz@gmail.com>
2024-09-11 07:58:55 -0300

    feat: delete addresse for EquitoVoteV0

--- addresses.ts
+++ addresses.ts
@@ -17,7 +17,6 @@ export const Addresses = {
 
 export const AddressesPerChain = {
   EthereumSepolia: {
-    EquitoVoteV0: "0x403A16CF04124FE6154e009c69a4e58A5B42F0fa" as Address,
     EquitoVoteV1: "0xEACAcDd65bbbDd15E7AcA41eF5311a33A8B9178c" as Address,
     EquitoVoteActive: "0xEACAcDd65bbbDd15E7AcA41eF5311a33A8B9178c" as Address,
 
@@ -25,7 +24,6 @@ export const AddressesPerChain = {
     VoteShereActive: "0x2ee891078cc2a08c31e494f19E36F772806b1613" as Address,
   },
   ArbitrumSepolia: {
-    EquitoVoteV0: "0x8e5eC33684DC8eE00A4Df0E8b58d279c9A9bdb3E" as Address,
     EquitoVoteV1: "0x45e0ce44717dbeeF610FCC2B45aaea40901AcB26" as Address,
     EquitoVoteActive: "0x45e0ce44717dbeeF610FCC2B45aaea40901AcB26" as Address,
 
@@ -33,7 +31,6 @@ export const AddressesPerChain = {
     VoteSphereActive: "0xC175b8abba483e57d36b7EBd9b4d3fBf630FECCA" as Address,
   },
   OptimismSepolia: {
-    EquitoVoteV0: "" as Address,
     EquitoVoteV1: "" as Address,
     EquitoVoteActive: "" as Address,
 
