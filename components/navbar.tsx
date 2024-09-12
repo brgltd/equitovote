3176aa1422b792bc81fa96f2d76e23efbfeb5e39
brgltd <brgltdxyz@gmail.com>
2024-09-12 18:24:56 -0300

    feat: add mobile responsiveness to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,14 +6,16 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 export function Navbar() {
   return (
     <nav>
-      <ul className="flex flex-row items-center mt-6 mb-8 ">
-        <li className="ml-12 mr-12 hover-glow">
-          <Link href="/">PROPOSALS</Link>
-        </li>
-        <li className="hover-glow">
-          <Link href="/create">CREATE</Link>
-        </li>
-        <li className="ml-auto mr-12">
+      <ul className="flex md:flex-row flex-col items-center mt-6 mb-8 justify-center">
+        <div className="flex flex-row mb-8 md:mb-0">
+          <li className="ml-0 md:ml-12 mr-12 hover-glow">
+            <Link href="/">PROPOSALS</Link>
+          </li>
+          <li className="hover-glow">
+            <Link href="/create">CREATE</Link>
+          </li>
+        </div>
+        <li className="md:ml-auto md:mr-12">
           <ConnectButton
             chainStatus="full"
             showBalance={false}
