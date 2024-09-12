5ebcef571a9be6c1a669e413bdd2e61f27b277a8
brgltd <brgltdxyz@gmail.com>
2024-09-12 17:32:34 -0300

    feat: remove navbar with materialui tabs and just use normal links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,11 +6,11 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 export function Navbar() {
   return (
     <nav>
-      <ul className="flex flex-row items-center mt-6 mb-8">
-        <li className="ml-12 mr-12">
+      <ul className="flex flex-row items-center mt-6 mb-8 ">
+        <li className="ml-12 mr-12 hover-glow">
           <Link href="/">PROPOSALS</Link>
         </li>
-        <li>
+        <li className="hover-glow">
           <Link href="/create">CREATE</Link>
         </li>
         <li className="ml-auto mr-12">
@@ -27,53 +27,3 @@ export function Navbar() {
     </nav>
   );
 }
-
-// import { usePathname } from "next/navigation";
-// import Link from "next/link";
-// import { Tabs, Tab } from "@mui/material";
-// import { SyntheticEvent, useEffect, useState } from "react";
-// import { ConnectButton } from "@rainbow-me/rainbowkit";
-
-// export function Navbar() {
-//   const [value, setValue] = useState<null | number>(null);
-//   const pathname = usePathname();
-
-//   useEffect(() => {
-//     if (pathname === "/") {
-//       setValue(0);
-//     } else if (pathname === "/create") {
-//       setValue(1);
-//     } else {
-//       setValue(null);
-//     }
-//   }, [pathname]);
-
-//   const handleChange = (event: SyntheticEvent, newValue: number) => {
-//     setValue(newValue);
-//   };
-
-//   return (
-//     <div className="flex md:flex-row flex-col md:items-start items-center mt-6 mb-8 justify-center">
-//       <div className="md:ml-12 mb-8 md:mb-8">
-//         <Tabs
-//           value={value}
-//           onChange={handleChange}
-//           aria-label="navigation tabs"
-//         >
-//           <Tab label="Proposals" component={Link} href="/" sx={{ mx: 1 }} />
-//           <Tab label="Create" component={Link} href="/create" sx={{ mx: 1 }} />
-//         </Tabs>
-//       </div>
-//       <div className="md:ml-auto md:mr-12">
-//         <ConnectButton
-//           chainStatus="full"
-//           showBalance={false}
-//           accountStatus={{
-//             smallScreen: "avatar",
-//             largeScreen: "full",
-//           }}
-//         />
-//       </div>
-//     </div>
-//   );
-// }
